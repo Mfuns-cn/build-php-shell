93ecb6c688341a0c2e16ae1eaaee3fdead1913d2
LixWorth <lixworth@gmail.com>
2022-03-10 15:04:03 +0800

    强烈重新运行 php80 升级swoole版本

--- php-80.sh
+++ php-80.sh
@@ -2,6 +2,7 @@ echo "Mfuns build php with swoole and more extensions on ubuntu-2004"
 
 base_dir=$(cd "$(dirname "$0")";pwd)
 
+echo "更新依赖"
 sudo apt update
 sudo apt install git wget autoconf automake bison build-essential curl flex \
   libtool libssl-dev libcurl4-openssl-dev libxml2-dev libreadline8 \
@@ -9,9 +10,14 @@ sudo apt install git wget autoconf automake bison build-essential curl flex \
   pkg-config re2c sqlite3 zlib1g-dev libonig5 libonig-dev libsodium-dev
 
 sudo apt install libboost-all-dev
+
+echo "删除旧环境"
+
 sudo rm -rf "${base_dir}/tmp/php80"
 sudo rm -rf /tmp/pecl/install
 sudo rm -rf /tmp/pear/install
+
+echo "开始编译 php-8.0.15"
 mkdir -p "${base_dir}/tmp/php80"
 cd "${base_dir}/tmp/php80" || exit
 wget https://www.php.net/distributions/php-8.0.15.tar.gz
@@ -42,6 +48,7 @@ make -j 12
 make install
 cp php.ini-development ${base_dir}/tmp/php80/php/lib/php.ini
 
+echo "开始编译 swoole"
 cd "${base_dir}/tmp/php80" || exit
 wget https://github.com/swoole/swoole-src/archive/refs/tags/v4.8.7.zip
 unzip v4.8.7.zip
@@ -51,6 +58,7 @@ cd "swoole-src-4.8.7" || exit
 make -s -j 12
 make install
 
+echo "开始编译 yasd debuger"
 # build yasd debuger
 cd "${base_dir}/tmp/php80" || exit
 wget https://github.com/swoole/yasd/archive/refs/tags/v0.3.9.zip
@@ -63,15 +71,19 @@ make clean && \
 make && \
 make install
 
+echo "开始初始化 pecl"
 wget http://pear.php.net/go-pear.phar
 sudo ${base_dir}/tmp/php80/php/bin/php go-pear.phar
 sudo ${base_dir}/tmp/php80/php/bin/pear config-get php_dir
 
 sudo ${base_dir}/tmp/php80/php/bin/pecl channel-update pecl.php.net
 #sudo ${base_dir}/tmp/php80/php/bin/pecl install zendopcache
+
+echo "开始安装 redis 与 libsodium(这是上游jwt库强制要求的 事实上这个包已纳入php内核)"
 sudo ${base_dir}/tmp/php80/php/bin/pecl install redis
 sudo ${base_dir}/tmp/php80/php/bin/pecl install libsodium
 
+echo "修改 phpini"
 echo "memory_limit=1G" >> ${base_dir}/tmp/php80/php/lib/php.ini
 echo "opcache.enable_cli = 'On'" >> ${base_dir}/tmp/php80/php/lib/php.ini
 echo "extension=redis.so" >> ${base_dir}/tmp/php80/php/lib/php.ini
@@ -91,11 +103,14 @@ echo "tracker.sampling_rate=100" >> ${base_dir}/tmp/php80/php/lib/php.ini
 echo "tracker.enable_memcheck=1" >> ${base_dir}/tmp/php80/php/lib/php.ini
 echo "tracker.enable=0" >> ${base_dir}/tmp/php80/php/lib/php.ini
 
+echo "初始化完成"
 ${base_dir}/tmp/php80/php/bin/php -v
 ${base_dir}/tmp/php80/php/bin/php -m
 ${base_dir}/tmp/php80/php/bin/php --ri swoole
 ${base_dir}/tmp/php80/php/bin/php --ri yasd
 
+
+echo "安装 composer "
 cd ${base_dir}/tmp/php80 || exit
 wget https://mirrors.aliyun.com/composer/composer.phar
 ${base_dir}/tmp/php80/php/bin/php composer.phar
